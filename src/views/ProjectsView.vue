2c8921ea1f5cf3b9c6765f7dc84e4469aa6abc71
Jacob Bastian <jcubby86@hotmail.com>
2022-08-16 21:43:58 -0600

    Use bootstrap for project styling

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="content projects" style="margin-bottom: 100px;">
-    <h1>Projects</h1>
-    <h2>CS 260</h2>
-    <div class="section">
-      <h3>Labs</h3>
-      <div class="section">
+  <div class="row" style="margin-bottom: 100px">
+    <div class="col-lg-2"></div>
+    <div class="col-lg-8 col-md-12">
+      <h1 class="section-header" id="section-0">CS 260</h1>
+      <div class="section-content">
+        <h2 class="section-header" id="section-0">Labs</h2>
         <div class="item">
           <img src="http://wildlife.jmbastian.com/images/photographer.jpg" />
           <div class="item-text">
@@ -54,48 +54,62 @@
             >
           </div>
         </div>
-      </div>
-      <h3>Creative Projects</h3>
-      <div class="section">        
-        <div class="item">
-          <img src="@/assets/images/star-wars.jpg" />
-          <div class="item-text">
-            <router-link to="/starwars"
-              ><h4>Star Wars Information</h4></router-link
-            >
-            <p>
-              This page uses a star wars api to show information about different
-              Star Wars movies!
-            </p>
+        <div class="section-content"></div>
+        <h2 class="section-header" id="section-0">Creative Projects</h2>
+        <div class="section-content">
+          <div class="item">
+            <img src="@/assets/images/star-wars.jpg" />
+            <div class="item-text">
+              <router-link to="/starwars"
+                ><h4>Star Wars Information</h4></router-link
+              >
+              <p>
+                This page uses a star wars api to show information about
+                different Star Wars movies!
+              </p>
+            </div>
           </div>
-        </div>
-        <div class="item">
-          <img src="@/assets/images/rubiks-cubes.jpg" />
-          <div class="item-text">
-            <a href="http://gallery.jmbastian.com"
-              ><h4>Rubik's Cubes</h4></a
-            >
-            <p>
-              I have been collecting Rubik's cubes since I was about 15 years old.
-            One day I was playing with one at a friend's house, and I was
-            determined to solve it. Once I did, I purchased one for myself and
-            began my collection, which has over 20 unique cubes in it today!
-            </p>
+          <div class="item">
+            <img src="@/assets/images/rubiks-cubes.jpg" />
+            <div class="item-text">
+              <a href="http://gallery.jmbastian.com"><h4>Rubik's Cubes</h4></a>
+              <p>
+                I have been collecting Rubik's cubes since I was about 15 years
+                old. One day I was playing with one at a friend's house, and I
+                was determined to solve it. Once I did, I purchased one for
+                myself and began my collection, which has over 20 unique cubes
+                in it today!
+              </p>
+            </div>
           </div>
-        </div>
-        <div class="item">
-          <img src="@/assets/images/collections.jpg" />
-          <div class="item-text">
-            <a href="http://collections.jmbastian.com"
-              ><h4>Collections</h4></a
-            >
-            <p>
-              I got the idea from the Rubik's Cubes project to make a site where I 
-              could fill it with all sorts of different collections!
-            </p>
+          <div class="item">
+            <img src="@/assets/images/collections.jpg" />
+            <div class="item-text">
+              <a href="http://collections.jmbastian.com"
+                ><h4>Collections</h4></a
+              >
+              <p>
+                I got the idea from the Rubik's Cubes project to make a site
+                where I could fill it with all sorts of different collections!
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    <div class="col-lg-2"></div>
   </div>
 </template>
+
+<style scoped>
+h1.section-header {
+  text-decoration: underline;
+}
+
+.section-content {
+  margin-bottom: 15px;
+}
+.section-content .section-content:not(:last-child) {
+  border-bottom: 1px solid darkgray;
+}
+</style>
