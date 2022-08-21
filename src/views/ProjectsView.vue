d489d824e13f637f7d0dcd23422fd7c77c04cca8
Jacob Bastian <jcubby86@hotmail.com>
2022-08-20 21:12:35 -0600

    Update some images, add games link

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -10,8 +10,7 @@
             <img class="p-2 img-fluid" src="http://wildlife.jmbastian.com/images/photographer.jpg" />
           </div>
           <div class="col-md-9">
-            <a href="http://wildlife.jmbastian.com"><h4>Lab 1 - Photographer's website</h4></a>
-            <p>Please forgive the colors - I was just learning</p>
+            <a target="_blank" href="http://wildlife.jmbastian.com"><h4>Photographer's website</h4></a>
           </div>
         </div>
 
@@ -20,7 +19,7 @@
             <img class="p-2 img-fluid" src="@/assets/images/weather.jpg" />
           </div>
           <div class="col-md-9">
-            <a href="http://weather.jmbastian.com"><h4>Lab 2 - Weather Forecast</h4></a>
+            <a target="_blank" href="http://weather.jmbastian.com"><h4>Weather Forecast</h4></a>
           </div>
         </div>
 
@@ -29,7 +28,7 @@
             <img class="p-2 img-fluid" src="https://imgs.xkcd.com/comics/hypothesis_generation.png" />
           </div>
           <div class="col-md-9">
-            <a href="http://xkcd.jmbastian.com"><h4>Lab 3a - Vue XKCD Browser</h4></a>
+            <a target="_blank" href="http://xkcd.jmbastian.com"><h4>XKCD Browser</h4></a>
           </div>
         </div>
 
@@ -38,7 +37,7 @@
             <img class="p-2 img-fluid" src="http://motherearth.jmbastian.com/images/logo.png" />
           </div>
           <div class="col-md-9">
-            <a href="http://motherearth.jmbastian.com"><h4>Lab 3b - Grocery Store</h4></a>
+            <a target="_blank" href="http://motherearth.jmbastian.com"><h4>Grocery Store</h4></a>
           </div>
         </div>
 
@@ -47,7 +46,7 @@
             <img class="p-2 img-fluid" src="@/assets/images/car.jpg" />
           </div>
           <div class="col-md-9">
-            <a href="http://museum.jmbastian.com"><h4>Lab 4 - Museum of Ordinary Objects</h4></a>
+            <a target="_blank" href="http://museum.jmbastian.com"><h4>Museum of Ordinary Objects</h4></a>
           </div>
         </div>
 
@@ -56,7 +55,7 @@
             <img class="p-2 img-fluid" src="@/assets/images/dog.jpg" />
           </div>
           <div class="col-md-9">
-            <a href="http://gallery.jmbastian.com"><h4>Lab 5 - Photobomb</h4></a>
+            <a target="_blank" href="http://gallery.jmbastian.com"><h4>Photobomb</h4></a>
           </div>
         </div>
 
@@ -86,7 +85,7 @@
             <img class="p-2 img-fluid" src="@/assets/images/rubiks-cubes.jpg" />
           </div>
           <div class="col-md-9">
-            <a href="http://rubiks.jmbastian.com"><h4>Rubik's Cubes</h4></a>
+            <a target="_blank" href="http://rubiks.jmbastian.com"><h4>Rubik's Cubes</h4></a>
             <p>
               I have been collecting Rubik's cubes since I was about 15 years
               old. One day I was playing with one at a friend's house, and I
@@ -102,10 +101,30 @@
             <img class="p-2 img-fluid" src="@/assets/images/collections.jpg" />
           </div>
           <div class="col-md-9">
-            <a href="http://collections.jmbastian.com"><h4>Collections</h4></a>
+            <a target="_blank" href="http://collections.jmbastian.com"><h4>Collections</h4></a>
                 <p>
                   I got the idea from the Rubik's Cubes project to make a site
                   where I could fill it with all sorts of different collections!
+                </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      
+    <div class="row" style="margin-bottom: 100px">
+
+      <div class="col">
+        <h2>Other Projects</h2>
+        
+        <div class="row">
+          <div class="col-md-3 col-6">
+            <img class="p-2 img-fluid" src="@/assets/images/game.jpg" />
+          </div>
+          <div class="col-md-9">
+            <a target="_blank" href="http://games.jmbastian.com"><h4>Games</h4></a>
+                <p>
+                  Inspired by jackbox games, I created a little site with some simple party games to play. My wife's family introduced me to these games and I've had a lot of fun playing them and making them easier to play together.
                 </p>
           </div>
         </div>
